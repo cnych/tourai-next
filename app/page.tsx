14e265c1ab6300e97ad4dc6507073d0276a3cd30
cnych <ych_1024@163.com>
2023-11-17 09:47:42 +0800

    change page style

--- app/page.tsx
+++ app/page.tsx
@@ -368,7 +368,7 @@ const Home: FC = () => {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col py-4">
       <div className="content">
         <div className="video-wrapper">
           <div>
